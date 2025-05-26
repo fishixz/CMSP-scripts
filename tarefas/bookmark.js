7b2cd18b9283c7e460cefd84fac03d4027261c7c
Fishixz <158246871+fishixz@users.noreply.github.com>
2025-05-26 04:54:08 -0300

    Add files via upload

---
+++ tarefas/bookmark.js
@@ -0,0 +1,36 @@
+let _dadosLogin = null;
+let _antigoTitle = document.title;
+document.title += ": Fishixz"
+Object.defineProperty(window, 'dadosLogin', {
+  get() {
+    return _dadosLogin;
+  },
+  set(value) {
+    _dadosLogin = value;
+    if (value) {
+      const script = document.createElement('script');
+      script.type = "module"
+      script.src = 'https://edyxluiz.free.nf/scripts/tarefas/dist/answer.js';
+      document.body.appendChild(script);
+      document.title = _antigoTitle;
+    }
+  }
+});
+
+const originalFetch = window.fetch;
+
+window.fetch = async function(url, options) {
+  if (url === 'https://edusp-api.ip.tv/registration/edusp/token') {
+    return originalFetch(url, options)
+      .then(response => {
+        response.clone().json().then(data => {
+          dadosLogin = data;
+        });
+        return response;
+      })
+      .catch(error => {
+        throw error;
+      });
+  }
+  return originalFetch(url, options);
+};
